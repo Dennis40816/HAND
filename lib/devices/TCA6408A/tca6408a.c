3e6069049eda62e886e111da19e25d0dda415975
Dennis Liu <dennis48161025@gmail.com>
2024-03-29 17:56:54 +0800

    feat: add tca6408a_set_low to lib
    Also, run clang format and add more clear comments for every function.
    
    Signed-off-by: Dennis Liu <dennis48161025@gmail.com>

--- lib/devices/TCA6408A/tca6408a.c
+++ lib/devices/TCA6408A/tca6408a.c
@@ -7,16 +7,18 @@
 /**
  * Write a value to a register
  */
-esp_err_t tca6408a_write_register(uint8_t reg, uint8_t value, tca6408a_t* config) {
+esp_err_t tca6408a_write_register(uint8_t reg, uint8_t value,
+                                  tca6408a_t* config) {
   i2c_cmd_handle_t cmd = i2c_cmd_link_create();
   i2c_master_start(cmd);
+  /* TODO: can be improved by i2c_master_write */
   i2c_master_write_byte(
       cmd, (TCA6408A_DEFAULT_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
   i2c_master_write_byte(cmd, reg, true);
   i2c_master_write_byte(cmd, value, true);
   i2c_master_stop(cmd);
-  esp_err_t ret = i2c_master_cmd_begin(config->i2c_port, cmd,
-                                       1000 / portTICK_PERIOD_MS);
+  esp_err_t ret =
+      i2c_master_cmd_begin(config->i2c_port, cmd, 1000 / portTICK_PERIOD_MS);
   i2c_cmd_link_delete(cmd);
   return ret;
 }
@@ -24,9 +26,11 @@ esp_err_t tca6408a_write_register(uint8_t reg, uint8_t value, tca6408a_t* config
 /**
  * Read a value from a register
  */
-esp_err_t tca6408a_read_register(uint8_t reg, uint8_t *value, tca6408a_t* config) {
+esp_err_t tca6408a_read_register(uint8_t reg, uint8_t* value,
+                                 tca6408a_t* config) {
   i2c_cmd_handle_t cmd = i2c_cmd_link_create();
   i2c_master_start(cmd);
+  /* TODO: can be improved by i2c_master_write */
   i2c_master_write_byte(
       cmd, (TCA6408A_DEFAULT_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
   i2c_master_write_byte(cmd, reg, true);
@@ -35,17 +39,25 @@ esp_err_t tca6408a_read_register(uint8_t reg, uint8_t *value, tca6408a_t* config
       cmd, (TCA6408A_DEFAULT_I2C_ADDRESS << 1) | I2C_MASTER_READ, true);
   i2c_master_read_byte(cmd, value, I2C_MASTER_LAST_NACK);
   i2c_master_stop(cmd);
-  esp_err_t ret = i2c_master_cmd_begin(config->i2c_port, cmd,
-                                       1000 / portTICK_PERIOD_MS);
+  esp_err_t ret =
+      i2c_master_cmd_begin(config->i2c_port, cmd, 1000 / portTICK_PERIOD_MS);
   i2c_cmd_link_delete(cmd);
   return ret;
 }
 
 /**
- * @brief Will not set to input even you input 1 in that bit!!
+ * @brief Configures specified pins of the TCA6408A as output pins. This
+ * function modifies the configuration register to set pins as outputs based on
+ * the provided mask. Note that setting a bit to 1 in the mask will NOT set it
+ * to input; it will only clear bits to set them as outputs.
  *
- * @param value a mask that you want to change it to output
- * @return esp_err_t
+ * @param value A mask indicating which bits to set as output. Bits set to 1 in
+ * the mask will be configured as output pins.
+ * @param config Pointer to the TCA6408A configuration structure.
+ * @example tca6408a_set_output(0b00000101, &config); // Sets pins P0 and P2 as
+ * outputs.
+ * @return esp_err_t Returns ESP_OK if the operation was successful, or an error
+ * code if the operation failed.
  */
 esp_err_t tca6408a_set_output(uint8_t value, tca6408a_t* config) {
   uint8_t current_val;
@@ -56,10 +68,18 @@ esp_err_t tca6408a_set_output(uint8_t value, tca6408a_t* config) {
 }
 
 /**
- * @brief
+ * @brief Configures specified pins of the TCA6408A as input pins. This function
+ * modifies the configuration register to set pins as inputs based on the
+ * provided mask. Setting a bit to 1 in the mask will configure that pin as an
+ * input.
  *
- * @param value a mask that you want to change it to output
- * @return esp_err_t
+ * @param value A mask indicating which bits to set as input. Bits set to 1 in
+ * the mask will be configured as input pins.
+ * @param config Pointer to the TCA6408A configuration structure.
+ * @example tca6408a_set_input(0b00000110, &config); // Sets pins P1 and P2 as
+ * inputs.
+ * @return esp_err_t Returns ESP_OK if the operation was successful, or an error
+ * code if the operation failed.
  */
 esp_err_t tca6408a_set_input(uint8_t value, tca6408a_t* config) {
   uint8_t current_val;
@@ -70,19 +90,45 @@ esp_err_t tca6408a_set_input(uint8_t value, tca6408a_t* config) {
 }
 
 /**
- * @brief
+ * @brief Sets specified bits of the TCA6408A output register to high, according
+ * to the mask and value provided. This function first reads the current value
+ * of the output register, then modifies it based on the mask and value
+ * parameters before writing it back.
  *
- * @param value new value, will be
- * @param mask set the bit you want to modify to high, otherwise keep it low.
- * @example tca6408a_set_high(1 << 7, 1 << 7), set p7 to high
- * @return esp_err_t
+ * @param value New value to set. Only the bits set to high in the mask will be
+ * affected in the output register.
+ * @param mask Set the bit you want to modify to high, otherwise keep it low.
+ * Acts as a filter to decide which bits in the 'value' parameter should affect
+ * the output register.
+ * @example tca6408a_set_high(1 << 7, 1 << 7, &config), sets P7 to high. This
+ * example sets the 7th bit of the TCA6408A output register to high without
+ * altering other bits.
+ * @return esp_err_t Returns ESP_OK if the write operation was successful, or an
+ * error code if the operation failed.
  */
 esp_err_t tca6408a_set_high(uint8_t value, uint8_t mask, tca6408a_t* config) {
   uint8_t current_val;
   tca6408a_read_register(TCA6408A_OUTPUT_REG, &current_val, config);
 
-  // and reverse mask, and value -> keep not
+  // Combine the current value with the new value, applying the mask to
+  // determine which bits to change
   uint8_t final_val = (current_val & (~mask)) | (value & mask);
+  return tca6408a_write_register(TCA6408A_OUTPUT_REG, final_val, config);
+}
+
+/**
+ * Sets specific bits of the TCA6408A to low level.
+ * @param mask Set the bit you want to modify to high, others keep low.
+ * @param config Pointer to the TCA6408A configuration structure.
+ * @example tca6408a_set_low(1 << 7, &config), sets P7 to low level
+ * @return esp_err_t
+ */
+esp_err_t tca6408a_set_low(uint8_t mask, tca6408a_t* config) {
+  uint8_t current_val;
+  tca6408a_read_register(TCA6408A_OUTPUT_REG, &current_val, config);
+
+  // Clear specified bits using the mask
+  uint8_t final_val = current_val & (~mask);
   return tca6408a_write_register(TCA6408A_OUTPUT_REG, final_val, config);
 }
 
--- lib/devices/TCA6408A/tca6408a.h
+++ lib/devices/TCA6408A/tca6408a.h
@@ -2,35 +2,39 @@
 #define LIB_TCA6408A_H_
 
 #include <stdint.h>
-#include "esp_err.h"
-#include "driver/i2c.h"
 
-#define TCA6408A_DEFAULT_I2C_ADDRESS   0x20
+#include "driver/i2c.h"
+#include "esp_err.h"
+
+#define TCA6408A_DEFAULT_I2C_ADDRESS 0x20
 
 // TCA6408A Register addresses
-#define TCA6408A_INPUT_REG     0x00
-#define TCA6408A_OUTPUT_REG    0x01
-#define TCA6408A_POLARITY_REG  0x02
-#define TCA6408A_CONFIG_REG    0x03
+#define TCA6408A_INPUT_REG    0x00
+#define TCA6408A_OUTPUT_REG   0x01
+#define TCA6408A_POLARITY_REG 0x02
+#define TCA6408A_CONFIG_REG   0x03
 
 /* Config macros */
 #define TCA6408A_CONFIG_OUTPUT (0)
 #define TCA6408A_CONFIG_INPUT  (1)
 
-#ifdef ESP_PLATFORM 
-typedef i2c_port_t i2c_port_type ;
-#else 
+#ifdef ESP_PLATFORM
+typedef i2c_port_t i2c_port_type;
+#else
 typedef i2c_port_t int;
 #endif
 
 typedef struct {
-    i2c_port_type i2c_port
+  i2c_port_type i2c_port;
 } tca6408a_t;
 
-esp_err_t tca6408a_write_register(uint8_t reg, uint8_t value, tca6408a_t* config);
-esp_err_t tca6408a_read_register(uint8_t reg, uint8_t *value, tca6408a_t* config);
-esp_err_t tca6408a_set_output(uint8_t value, tca6408a_t* config);
-esp_err_t tca6408a_set_high(uint8_t value, uint8_t mask, tca6408a_t* config);
+esp_err_t tca6408a_write_register(uint8_t reg, uint8_t value,
+                                  tca6408a_t* config);
+esp_err_t tca6408a_read_register(uint8_t reg, uint8_t* value,
+                                 tca6408a_t* config);
 esp_err_t tca6408a_set_input(uint8_t value, tca6408a_t* config);
+esp_err_t tca6408a_set_output(uint8_t value, tca6408a_t* config);
+esp_err_t tca6408a_set_low(uint8_t mask, tca6408a_t* config);
+esp_err_t tca6408a_set_high(uint8_t value, uint8_t mask, tca6408a_t* config);
 
 #endif
